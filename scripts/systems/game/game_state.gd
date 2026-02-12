eebe709ce73b710a428c4f75a4d346096fb690d9
Nick Thorby <nick@exonic.co.za>
2026-02-12 15:07:47 +0200

    feat: New State + Data Classes

--- scripts/systems/game/game_state.gd
+++ scripts/systems/game/game_state.gd
@@ -7,7 +7,7 @@ var tamer_id: StringName = &""
 var play_time: int = 0
 
 var party: PartyState = PartyState.new()
-var storage: Array[DigimonState] = []
+var storage: StorageState = StorageState.new()
 var inventory: InventoryState = InventoryState.new()
 var story_flags: Dictionary = {}
 ## Digimon key -> scan progress (0.0-1.0).
@@ -15,16 +15,12 @@ var scan_log: Dictionary = {}
 
 
 func to_dict() -> Dictionary:
-	var storage_dicts: Array[Dictionary] = []
-	for digimon: DigimonState in storage:
-		storage_dicts.append(digimon.to_dict())
-
 	return {
 		"tamer_name": tamer_name,
 		"tamer_id": tamer_id,
 		"play_time": play_time,
 		"party": party.to_dict(),
-		"storage": storage_dicts,
+		"storage": storage.to_dict(),
 		"inventory": inventory.to_dict(),
 		"story_flags": story_flags.duplicate(),
 		"scan_log": scan_log.duplicate(),
@@ -37,11 +33,8 @@ static func from_dict(data: Dictionary) -> GameState:
 	state.tamer_id = StringName(data.get("tamer_id", ""))
 	state.play_time = data.get("play_time", 0)
 	state.party = PartyState.from_dict(data.get("party", {}))
+	state.storage = StorageState.from_dict(data.get("storage", {}))
 	state.inventory = InventoryState.from_dict(data.get("inventory", {}))
 	state.story_flags = data.get("story_flags", {})
 	state.scan_log = data.get("scan_log", {})
-
-	for digimon_data: Dictionary in data.get("storage", []):
-		state.storage.append(DigimonState.from_dict(digimon_data))
-
 	return state
